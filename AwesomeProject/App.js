778313e2174ce87f9e9a4aea0d3e6effc38b89fa
SuranartN <suranartnc@gmail.com>
2017-10-21 15:00:31 +0700

    add SectionList

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -17,25 +17,22 @@ import {
   Alert,
   TouchableHighlight,
   ScrollView,
-  FlatList
+  FlatList,
+  SectionList
 } from 'react-native';
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <FlatList
-          data={[
-            { key: 'Devin' },
-            { key: 'Jackson' },
-            { key: 'James' },
-            { key: 'Joel' },
-            { key: 'John' },
-            { key: 'Jillian' },
-            { key: 'Jimmy' },
-            { key: 'Julie' },
+        <SectionList
+          sections={[
+            { title: 'Section 1', data: ['Devin'] },
+            { title: 'Section 2', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie'] },
           ]}
-          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item, index) => index}
         />
       </View>
     );
@@ -46,14 +43,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 22,
-    // borderColor: 'blue',
-    // borderWidth: 3,
    },
-   item: {
-     padding: 10,
-     fontSize: 18,
-     height: 44,
-     borderColor: '#aaa',
-     borderWidth: 1,
-   },
+   sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 18,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 14,
+    height: 44,
+  },
 });
