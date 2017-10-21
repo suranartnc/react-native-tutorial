07212d42eb466ec16e470fd09b97319f7ffb2810
SuranartN <suranartnc@gmail.com>
2017-10-21 14:57:01 +0700

    add FlatList

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -16,109 +16,44 @@ import {
   Button,
   Alert,
   TouchableHighlight,
-  ScrollView 
+  ScrollView,
+  FlatList
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component<{}> {
-  constructor(props) {
-    super(props);
-    this.handleKeywordsChanged = this.handleKeywordsChanged.bind(this);
-    this.handleKeywordsSubmitted = this.handleKeywordsSubmitted.bind(this);
-    
-    this.state = {
-      show: true,
-      keywords: '',
-      output: 'default'
-    };
-  }
-
-  componentDidMount() {
-    // setInterval(() => {
-    //   this.setState({
-    //     show: !this.state.show
-    //   })
-    // }, 1000);
-  }
-  
-  handleKeywordsChanged(keywords) {
-    this.setState({
-      keywords
-    });
-  }
-
-  handleKeywordsSubmitted() {
-    this.setState({
-      output: this.state.keywords
-    });
-  }
-
   render() {
     return (
-      <ScrollView contentContainerStyle={styles.container}>
-        <View>
-          <View style={styles.highlight}>
-            <Text>{this.state.output}</Text>
-            <TouchableHighlight onPress={() => { Alert.alert('TouchableHighlight'); }}>
-              <View>
-                <Text>TouchableHighlight</Text>
-              </View>
-            </TouchableHighlight>
-            <Button 
-              title={this.state.keywords} 
-              onPress={() => { Alert.alert(this.state.keywords); }}
-            />
-            {/* { 
-              this.state.show && 
-              <Image 
-                source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }}
-                style={styles.image}
-              /> 
-            } */}
-          </View>
-          <View style={styles.highlight} />
-            <TextInput 
-              placeholder="Enter keywords here..." 
-              onChangeText={this.handleKeywordsChanged} 
-              onSubmitEditing={this.handleKeywordsSubmitted}
-            />
-          <View style={styles.highlight} />
-          <View style={styles.highlight} />
-          <View style={styles.highlight} />
-          <View style={styles.highlight} />
-          <View style={styles.highlight} />
-          <View style={styles.highlight} />
-          <View style={styles.highlight} />
-          <View style={styles.highlight} />
-          <View style={styles.highlight} />
-
-        </View>
-      </ScrollView>
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            { key: 'Devin' },
+            { key: 'Jackson' },
+            { key: 'James' },
+            { key: 'Joel' },
+            { key: 'John' },
+            { key: 'Jillian' },
+            { key: 'Jimmy' },
+            { key: 'Julie' },
+          ]}
+          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#aaa',
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-  },
-  highlight: {
-    borderWidth: 1,    
-    borderColor: 'red',
-    width: 200,
-    height: 100
-  },
-  image: {
-    width: 193,
-    height: 110
-  }
+    flex: 1,
+    paddingTop: 22,
+    // borderColor: 'blue',
+    // borderWidth: 3,
+   },
+   item: {
+     padding: 10,
+     fontSize: 18,
+     height: 44,
+     borderColor: '#aaa',
+     borderWidth: 1,
+   },
 });
